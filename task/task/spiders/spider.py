e7d78b2a66bf648ce4a92556d1208be1e1cbd039
anujasibi <45849232+anujasibi@users.noreply.github.com>
2018-12-23 17:27:38 +0530

    Update spider.py

--- task/task/spiders/spider.py
+++ task/task/spiders/spider.py
@@ -14,13 +14,13 @@ class SpiderSpider(scrapy.Spider):
     def parse(self, response):
         links=response.css('div.row a.v-pa-list-link::attr("href")').extract()
         for link in links: 
-            url=response.urljoin('http://www.avvo.com/all-lawyers/ny/new_york.html'+link)
+            url=response.urljoin(link)
             yield scrapy.Request(url, callback=self.parse_new)
            
     def parse_new(self, response):  
          news=response.css('div.row a.v-serp-block-link::attr("href")').extract()
          for new in news:
-            newu =response.urljoin(url+new)
+            newu =response.urljoin(new)
             yield scrapy.Request(url, callback=self.parse_details)
     def parse_details(self, response):
         yield{
